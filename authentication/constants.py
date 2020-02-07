95e995c533cd20beac52b7f07c04e25baf8ad6b9
amirdehghan <amir.dehghan15@yahoo.com>
2020-02-08 00:28:13 +0330

    New Github

---
+++ authentication/constants.py
@@ -0,0 +1,11 @@
+MAJORS = [
+    ('ce', 'Computer Engineering'),
+    ('cs', 'Conputer Science'),
+    ('ee', 'Electrical Engineering')
+]
+
+RANKS = [
+    ('AO', 'Associate Professor'),
+    ('AI', 'Assistant Professor'),
+    ('PR', 'Professor')
+]
